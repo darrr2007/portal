43433f6f1e9ad2c0ece83b223ee91052e444224e
Dharshni <dharshnijaya@gmail.com>
2023-10-23 12:59:30 +0530

    update

--- src/Portal.js
+++ src/Portal.js
@@ -1,93 +1,99 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Link, Switch } from "react-router-dom";
+import {
+  Link,
+  useNavigate,
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 import "./Portal.css";
-import PdfPreview from "./PdfPreview.js";
-
 
 const Portal = () => {
-      return (
-        <BrowserRouter>
-        <div className="portal">
-        <b className="all-rights-reserved"> Streak Tech | All rights reserved | 2023 </b>
-        <div className="contentwrap">
-          <img className="logo-icon" alt="" src="./nfo.svg" />
-          <div className="contentwrap1">
-            <div className="pagetitle">
-              <b className="nfo-content">NFO Content</b>
-              <img className="titleline-icon" alt="" src="/titleline.svg" />
-            </div>
-            <div className="guide-butt">
-              <img className="img-icon" alt="" src="./Guide-Img.png" />
-              <div className="content">
-                <div className="nfo-content">6th grade <br/> Guidebook</div>
-                <button className="button">
-                    <b className="sml-txt" >View</b>
-                    
-
-                </button>
+  const navigate = useNavigate();
+  return (
+    <div className="portal">
+      <b className="all-rights-reserved">
+        {" "}
+        Streak Tech | All rights reserved | 2023{" "}
+      </b>
+      <div className="contentwrap">
+        <img className="logo-icon" alt="" src="./nfo.svg" />
+        <div className="contentwrap1">
+          <div className="pagetitle">
+            <b className="nfo-content">NFO Content</b>
+            <img className="titleline-icon" alt="" src="/titleline.svg" />
+          </div>
+          <div className="guide-butt">
+            <img className="img-icon" alt="" src="./Guide-Img.png" />
+            <div className="content">
+              <div className="nfo-content">
+                6th grade <br /> Guidebook
               </div>
+              <button
+                className="button"
+                onClick={() => {
+                  navigate("/pdf-preview");
+                }}
+              >
+                <b className="sml-txt">View</b>
+              </button>
             </div>
-            <div className="guide-butt1">
-              <img className="img-icon" alt="" src="./Workbook-img.png" />
-              <div className="content">
-                <div className="nfo-content">6th grade <br/> Workbook</div>
-                <button className="button">
-                    <b className="sml-txt"> <Link to="/PdfPreview">View</Link></b>
-                    
-                </button>
+          </div>
+          <div className="guide-butt1">
+            <img className="img-icon" alt="" src="./Workbook-img.png" />
+            <div className="content">
+              <div className="nfo-content">
+                6th grade <br /> Workbook
               </div>
+              <button className="button">
+                <b className="sml-txt">View</b>
+              </button>
             </div>
-            <div className="guide-butt2">
-              <img className="img-icon" alt="" src="./advn.png" />
-              <div className="content2">
-                <div className="nfo-content">6th National Guidebook</div>
-                <button className="buy-button">
-                    <b className="sml-txt1" >Buy Guide</b>
-                </button>
-              </div>
-            </div>
-            <div className="examinfo">
-              <div className="content3">
-                <div className="nfo-content">Your exam is scheduled on</div>
-                <b className="examdate">
-                  <p className="am-to-1130am">{`19th November 2023, `}</p>
-                  <p className="am-to-1130am">10am to 11:30am</p>
-                </b>
-              </div>
+          </div>
+          <div className="guide-butt2">
+            <img className="img-icon" alt="" src="./advn.png" />
+            <div className="content2">
+              <div className="nfo-content">6th National Guidebook</div>
               <button className="buy-button">
-                    <b className="sml-txt1" >Exam Rules</b>
-            </button>
+                <b className="sml-txt1">Buy Guide</b>
+              </button>
             </div>
           </div>
-          <div className="studentid-wrap">
-            <div className="studentid">
-              <b className="name">Sauraj Babu</b>
-              <div className="line" />
-              <div className="grade">6th Grade</div>
-              <div className="line" />
-              <div className="grade">
-                <p className="am-to-1130am">Bhavans Vidya Mandir </p>
-                <p className="am-to-1130am">Nadavarambhu, Irinjalakuda</p>
-              </div>
-              <div className="line3" />
-              <div className="sign-out">Sign Out of NFO</div>
+          <div className="examinfo">
+            <div className="content3">
+              <div className="nfo-content">Your exam is scheduled on</div>
+              <b className="examdate">
+                <p className="am-to-1130am">{`19th November 2023, `}</p>
+                <p className="am-to-1130am">10am to 11:30am</p>
+              </b>
             </div>
-            <button className="supportbutton">
-              <img className="vector-icon" alt="" src="./phone-icon.png" />
-              <b className="support-text">Call Support</b>
+            <button className="buy-button">
+              <b className="sml-txt1">Exam Rules</b>
             </button>
           </div>
-          <Routes>
-            <Route exact path="/PdfPreview" element={<PdfPreview />} />
-          </Routes>
         </div>
-         
+        <div className="studentid-wrap">
+          <div className="studentid">
+            <b className="name">Sauraj Babu</b>
+            <div className="line" />
+            <div className="grade">6th Grade</div>
+            <div className="line" />
+            <div className="grade">
+              <p className="am-to-1130am">Bhavans Vidya Mandir </p>
+              <p className="am-to-1130am">Nadavarambhu, Irinjalakuda</p>
+            </div>
+            <div className="line3" />
+            <div className="sign-out">Sign Out of NFO</div>
+          </div>
+          <button className="supportbutton">
+            <img className="vector-icon" alt="" src="./phone-icon.png" />
+            <b className="support-text">Call Support</b>
+          </button>
+        </div>
       </div>
-      </BrowserRouter>
-
-      );
-
-    };
+    </div>
+  );
+};
 
 export default Portal;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './index.css';
-// import App from './App';
-// import CreateAccount from './CreateAccount';
-// import Login from './Login';
-// import Portal from './Portal'
+import App from './App';
+import CreateAccount from './CreateAccount';
+import Login from './Login';
+import Portal from './Portal'
 import PdfPreview from './PdfPreview';
 import Logout from './Logout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Portal />}>
+    <Route path="/pdf-preview" element={<PdfPreview />} />
+    </Route>
+    </Routes>
       
-      <Logout />
       
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
